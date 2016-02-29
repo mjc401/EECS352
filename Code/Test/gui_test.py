c0fac2fa9269ebddbb2103e2b6e77bd7c0ac5d8e
mjcollins <MatthewCollins2016@u.northwestern.edu>
2016-02-28 22:20:23 -0600

    Added more buttons to GUI
    Save buttons for recorded audio and output

--- Code/Test/gui_test.py
+++ Code/Test/gui_test.py
@@ -26,6 +26,17 @@ class Application(Frame):
 		sr = self.sr_entry.get()
 		sr = int(float(sr))
 		os.system('python test_aubio.py %s %d' % (wave_path,sr))
+		
+	def rb_select(self):
+		rb_sel =  rbvar.get()
+		if rb_sel is 1:
+			self.record_button["state"] = DISABLED
+			self.wpath["state"] = NORMAL
+			self.input_mb["state"] = DISABLED
+		if rb_sel is 2:
+			self.record_button["state"] = NORMAL
+			self.wpath["state"] = DISABLED
+			self.input_mb["state"] = NORMAL
 
 	def createWidgets(self): # Create Widgets
 		# QUIT Button
@@ -43,13 +54,16 @@ class Application(Frame):
 		self.record_button.pack({"side": "left"})
 		
 		# Stop Button
-		self.stop_button = Button(self, text="Stop").pack({"side": "left"})
+		self.stop_button = Button(self, text="Stop")
+		self.stop_button.pack({"side": "left"})
 		
 		# Run Button
-		self.run_button = Button(self, text="Run", command=self.run_pitch_track).pack({"side": "left"})
+		self.run_button = Button(self, text="Run", command=self.run_pitch_track)
+		self.run_button.pack({"side": "left"})
 		
 		# WAV File Load Button
-		self.wpath = Button(self, text="Load WAV File",command=self.wav_file_path).pack({"side": "left"})
+		self.wpath = Button(self, text="Load WAV File",command=self.wav_file_path)
+		self.wpath.pack({"side": "left"})
 		self.wptext = Text(self, height=1, width=40)
 		self.wptext.insert(INSERT, "WAV Path")
 		self.wptext.pack({"side": "left"})
@@ -59,6 +73,35 @@ class Application(Frame):
 		self.sr_entry = Entry(self)
 		self.sr_entry.insert(0, 44100)
 		self.sr_entry.pack({"side": "left"})
+		
+		# Input Selections		
+		global mbvar
+		mbvar = IntVar()
+		self.input_mb = Menubutton(self, text="Select Input")
+		self.input_mb.grid()
+		self.input_mb.menu = Menu(self.input_mb, tearoff=0)
+		self.input_mb["menu"]  =  self.input_mb.menu
+		self.input_mb.menu.add_radiobutton(label="Mic", variable=mbvar, value=1)
+		self.input_mb.menu.add_radiobutton(label="External Instrument", variable=mbvar, value=2)
+		
+		global rbvar
+		rbvar = IntVar()
+		self.file_rb = Radiobutton(self, text="File Upload", variable=rbvar, value=1, command=self.rb_select)
+		self.file_rb.pack(anchor = NW)
+		self.file_rb.invoke()
+		self.record_rb = Radiobutton(self, text="Record", variable=rbvar, value=2, command=self.rb_select)
+		self.record_rb.pack(anchor = NW)
+		#self.live_rb = Radiobutton(self, text="Live", variable=var, value=3)
+		
+		self.input_mb.pack({"side": "left"})
+		
+		# Save Buttons
+		self.save_rec = Button(self, text="Save Recorded Audio", state=DISABLED)
+		self.save_rec.pack({"side": "left"})
+		
+		self.save_out = Button(self, text="Save Output", state=DISABLED)
+		self.save_out.pack({"side": "left"})
+		
 		
 		
 	def __init__(self, master=None):
