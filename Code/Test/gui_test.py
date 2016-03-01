ee6abfc70b689be66763a37a2dbc6c38e21efa10
mjcollins <MatthewCollins2016@u.northwestern.edu>
2016-03-01 15:45:06 -0600

    Updated GUI layout

--- Code/Test/gui_test.py
+++ Code/Test/gui_test.py
@@ -1,7 +1,7 @@
 from Tkinter import *
 import tkFileDialog # Get file path 
 #from Record import record_audio, stop_record
-import os, sys, numpy as np, pyaudio, wave
+import os, sys, numpy as np, pyaudio, wave, tkFont
 
 
 # Plot in GUI
@@ -32,8 +32,6 @@ class Application(Frame):
 
 		self.rec_data = []
 		self.rec_data_np = []
-		
-		self.rec_end = 0
 	
 		while self.rec_on is True: # Record
 			rec_samps = rec_stream.read(num_samps)
@@ -42,14 +40,12 @@ class Application(Frame):
 			root.update() # make sure GUI can still update & not get stuck in loop
 
 		self.rec_data_np = np.hstack(self.rec_data_np) # output numpy data array
-		librosa.output.write_wav('test_np.wav', self.rec_data_np, 44100, norm=False)
-		print len(self.rec_data_np)
+		#librosa.output.write_wav('test_np.wav', self.rec_data_np, 44100, norm=False)
+
 	# Close Stream
 		rec_stream.stop_stream()
 		rec_stream.close()
 		audio_rec.terminate()
-		
-		self.rec_end = 1
 
 		waveFile = wave.open("test_rec.wav", 'wb')
 		waveFile.setnchannels(1)
@@ -68,9 +64,9 @@ class Application(Frame):
 		
 	# Run Aubio Demo
 	def run_pitch_track(self):
-		sr = self.sr_entry.get()
-		sr = int(float(sr))
-		os.system('python test_aubio.py %s %d' % (self.wave_path,sr))
+		self.sr = self.sr_entry.get()
+		self.sr = int(float(self.sr))
+		os.system('python test_aubio.py %s %d' % (self.wave_path,self.sr))
 
 	# Run InstruSwitch
 #	def run_instruswitch(self):
@@ -78,13 +74,13 @@ class Application(Frame):
 	
 	# Input Select		
 	def rb_select(self):
-		rb_sel =  rbvar.get()
-		if rb_sel is 1: # File Upload Selected
+		self.rb_sel =  self.rbvar.get()
+		if self.rb_sel is 1: # File Upload Selected
 			self.record_button["state"] = DISABLED
 			self.sr_entry["state"] = NORMAL
 			self.wpath["state"] = NORMAL
 			self.input_mb["state"] = DISABLED
-		if rb_sel is 2: # Record Selected
+		if self.rb_sel is 2: # Record Selected
 			self.record_button["state"] = NORMAL
 			self.sr_entry["state"] = DISABLED
 			self.wpath["state"] = DISABLED
@@ -92,38 +88,36 @@ class Application(Frame):
 			
 	# Record
 	def record_start(self):
-		mb_sel =  mbvar.get()
-		if mb_sel == 1: # Mic as Input
+		self.mb_sel =  self.mbvar.get()
+		if self.mb_sel == 1: # Mic as Input
 			self.rec_on = True
 			print "Recording..."
 			self.record_audio(0)
-		#if mb_sel == 2: # External as Input
+		#if self.mb_sel == 2: # External as Input
 		#	Rec_on = True
 		#	print "Recording..."
 		#	record_audio(1)
 		
 	# Stop
-	def stop_rec_play(self):
+	def stop_rec(self):
 		if self.rec_on is True:
 			self.rec_on = False
 			print "Finished Recording"
 			self.play_rec["state"] = NORMAL
 			self.save_rec["state"] = NORMAL
-			rb_sel =  rbvar.get()
-			#while self.rec_end is 0: pass
-	
-				
-		#if play_on is True:
+		#if self.play_on is True:
+			#self.play_on = False
 	
 	#def save_file(self):
+	#	self.save_path = self.save_entry.get()
+
 	
 	def plot_input(self):
 		fig1 = Figure(figsize=(5,5), dpi=100)
 		a1 = fig1.add_subplot(111)
-		rb_sel =  rbvar.get()
-		if rb_sel is 1:
+		if self.rb_sel is 1:
 			1
-		if rb_sel is 2:
+		if self.rb_sel is 2:
 			t_length = len(self.rec_data_np)
 			times = np.arange(0,t_length * 44100)/44100.
 			print len(self.rec_data_np), times.shape
@@ -142,77 +136,127 @@ class Application(Frame):
 		self.QUIT["text"] = "QUIT"
 		self.QUIT["fg"]   = "red"
 		self.QUIT["command"] =  self.quit
-		self.QUIT.pack({"side": "left"})
+		self.QUIT.grid(row=0, column=0, sticky=NW)
+	
+	# Title
+		self.title_font = tkFont.Font(family="Helvetica", size=36, weight="bold")
+		self.instruswitch_title = Label(self, text="InstruSwitch", font=self.title_font, bg="grey85")
+		self.instruswitch_title.grid(row=0, columnspan=20, ipady=25)
+	
+	# Inputs
+		self.input_font = tkFont.Font(family="Helvetica", size=24, weight="bold")
+		self.input_label = Label(self, text="Select Inputs:                     ", bg= "cornflower blue", font=self.input_font)
+		self.input_label.grid(row=1, columnspan=10, ipady=10, sticky=W, pady=5)
+		
+		# Upload, Record, Live
+		self.rbvar = IntVar()
+		self.file_rb = Radiobutton(self, text="File Upload", variable=self.rbvar, value=1, command=self.rb_select, bg="grey85")
+		self.file_rb.grid(row=2, column=0, sticky=W, ipady=5)
+		self.record_rb = Radiobutton(self, text="Record", variable=self.rbvar, value=2, command=self.rb_select, bg="grey85")
+		self.record_rb.grid(row=3, column=0, sticky=W, ipady=5)
+		self.live_rb = Radiobutton(self, text="Live", variable=self.rbvar, value=3, state=DISABLED, bg="grey85")
+		self.live_rb.grid(row=4, column=0, sticky=W, ipady=5)
+		
+		# WAV File Load Button
+		self.wpath = Button(self, text="Load WAV File",command=self.wav_file_path)
+		self.wpath.grid(row=2, column=1)
+		self.wptext = Text(self, height=1, width=40)
+		self.wptext.insert(INSERT, "WAV Path")
+		self.wptext.grid(row=2, column=2, columnspan=5)
+	
+		# Sample Rate Entry
+		self.sr_label = Label(self, text="Sample Rate (Hz)", bg="grey85")
+		self.sr_label.grid(row=2, column=8, columnspan=5, ipadx=5)
+		self.sr_entry = Entry(self)
+		self.sr_entry.insert(0, 44100)
+		self.sr_entry.grid(row=2, column=14, columnspan=5)
+		
+		# Mic, External, Etc.		
+		self.mbvar = IntVar()
+		self.input_mb = Menubutton(self, text="Select Input")
+		self.input_mb.grid(row=3, column=1)
+		self.input_mb.menu = Menu(self.input_mb, tearoff=0)
+		self.input_mb["menu"]  =  self.input_mb.menu
+		self.input_mb.menu.add_radiobutton(label="Mic", variable=self.mbvar, value=1)
+		self.input_mb.menu.add_radiobutton(label="External Instrument", variable=self.mbvar, value=2)
+
 		
 		# Record Button
 		self.record_button = Button(self)
 		self.record_button["text"] = "Record",
 		self.record_button["command"] = self.record_start
 		self.record_button["activeforeground"] = "red"
-		self.record_button.pack({"side": "left"})
+		self.record_button.grid(row=3, column=2)
 		
-		# Stop Button
-		self.stop_button = Button(self, text="Stop", command=self.stop_rec_play)
-		self.stop_button.pack({"side": "left"})
+		# Stop Button (Record)
+		self.stop_button = Button(self, text="Stop", command=self.stop_rec)
+		self.stop_button.grid(row=3, column=3, sticky=W)
+		
+	# Select Instrument Configuration
+		self.instr_font = tkFont.Font(family="Helvetica", size=24, weight="bold")
+		self.instr_label = Label(self, text="Select Output Instrument:", bg= "cornflower blue", font=self.instr_font)
+		self.instr_label.grid(row=5, columnspan=8, ipady=10, sticky=W, pady=5)
+		
+		# Instrument Menu
+		self.ibvar = IntVar()
+		self.input_inst = Menubutton(self, text="Select Instrument")
+		self.input_inst.grid(row=6, column=0, sticky=W)
+		self.input_inst.menu = Menu(self.input_inst, tearoff=0)
+		self.input_inst["menu"]  =  self.input_inst.menu
+		self.input_inst.menu.add_radiobutton(label="Piano", variable=self.ibvar, value=1)
+		self.input_inst.menu.add_radiobutton(label="Violin", variable=self.ibvar, value=2)
+		self.input_inst.menu.add_radiobutton(label="Trumpet", variable=self.ibvar, value=3)
+		self.input_inst.menu.add_radiobutton(label="Bass", variable=self.ibvar, value=4)
+		
+	# Run, Play, & Save
+		
+		self.rps_font = tkFont.Font(family="Helvetica", size=24, weight="bold")
+		self.rps_label = Label(self, text="Run, Play, & Save:", bg= "cornflower blue", font=self.rps_font, anchor=W)
+		self.rps_label.grid(row=5, column=8, columnspan=18, ipady=10, sticky=W+E, pady=5)
 		
 		# Run Button
-		self.run_button = Button(self, text="Run", command=self.run_pitch_track)
-		self.run_button.pack({"side": "left"})
-		
-		# WAV File Load Button
-		self.wpath = Button(self, text="Load WAV File",command=self.wav_file_path)
-		self.wpath.pack({"side": "left"})
-		self.wptext = Text(self, height=1, width=40)
-		self.wptext.insert(INSERT, "WAV Path")
-		self.wptext.pack({"side": "left"})
-	
-		# Sample Rate Entry
-		self.sr_label = Label(self, text="Sample Rate (Hz)").pack({"side": "left"})
-		self.sr_entry = Entry(self)
-		self.sr_entry.insert(0, 44100)
-		self.sr_entry.pack({"side": "left"})
-		
-		# Input Selections		
-		global mbvar
-		mbvar = IntVar()
-		self.input_mb = Menubutton(self, text="Select Input")
-		self.input_mb.grid()
-		self.input_mb.menu = Menu(self.input_mb, tearoff=0)
-		self.input_mb["menu"]  =  self.input_mb.menu
-		self.input_mb.menu.add_radiobutton(label="Mic", variable=mbvar, value=1)
-		self.input_mb.menu.add_radiobutton(label="External Instrument", variable=mbvar, value=2)
-		
-		global rbvar
-		rbvar = IntVar()
-		self.file_rb = Radiobutton(self, text="File Upload", variable=rbvar, value=1, command=self.rb_select)
-		self.file_rb.pack(anchor = NW)
-		self.file_rb.invoke()
-		self.record_rb = Radiobutton(self, text="Record", variable=rbvar, value=2, command=self.rb_select)
-		self.record_rb.pack(anchor = NW)
-		#self.live_rb = Radiobutton(self, text="Live", variable=var, value=3)
-		
-		self.input_mb.pack({"side": "left"})
-		
-		# Save Buttons
-		self.save_label = Label(self, text="Save Path").pack({"side": "left"})
-		self.save_entry = Entry(self)
-		self.save_entry.pack({"side": "left"})
-		
-		self.save_rec = Button(self, text="Save Recorded Audio", state=DISABLED)
-		self.save_rec.pack({"side": "left"})
-		
-		self.save_out = Button(self, text="Save Output", state=DISABLED)
-		self.save_out.pack({"side": "left"})
+		self.run_button = Button(self, text="Run InstruSwitch", command=self.run_pitch_track)
+		self.run_button.grid(row=6, column=8, columnspan=3, pady=5)
 		
 		# Play Button
-		self.play_rec = Button(self, text="Play Recording", state=DISABLED)
-		self.play_rec.pack({"side": "left"})
-		self.play_out = Button(self, text="Play Output", state=DISABLED)
-		self.play_out.pack({"side": "left"})
+		self.play_button = Button(self, text="Play", state=DISABLED)
+		self.play_button.grid(row=7, column=8, sticky=W, pady=5)
 		
+		# Stop Button (Audio)
+		self.stop_audio_button = Button(self, text="Stop", state=DISABLED)#, command=self.stop_play)
+		self.stop_audio_button.grid(row=7, column=9, sticky=W, pady=5)
+		
+		# Original or Output Select
+		self.playbvar = IntVar()
+		self.play_orig_rb = Radiobutton(self, text="Original", variable=self.playbvar, value=1, bg="grey85")
+		self.play_orig_rb.grid(row=7, column=15, sticky=W, ipady=5)
+		self.play_out_rb = Radiobutton(self, text="Output", variable=self.playbvar, value=2, bg="grey85")
+		self.play_out_rb.grid(row=7, column=17, sticky=W, ipady=5)
+		
+		# Save Buttons
+		self.save_button = Button(self, text="Save", state=DISABLED)
+		self.save_button.grid(row=8, column=8, sticky=W, pady=5)
+		
+		#self.save_label = Label(self, text="Save Path")
+		#self.save_label.grid(row=8, column=9, columnspan=3)
+		self.save_entry = Entry(self, width=30)
+		self.save_entry.insert(INSERT, "Save Path")
+		self.save_entry.grid(row=8, column=10, columnspan=15, sticky=W, pady=5)
+	
+		
+		
+		# Save/Play Selection
+		#self.play_var = IntVar()
+		#self.play_orig = Radiobutton(self, text="Play Original", variable=self.play_var, value=1, command=self.play_select)
+		#self.play_orig.pack(anchor = NW)
+		#self.play_out = Radiobutton(self, text="Play Output", variable=self.play_var, value=2, command=self.play_select)
+		#self.play_out.pack(anchor = NW)
+		
+	# Plots, etc.
 
-		
-		
+	# Initialize
+		self.file_rb.invoke()
+		self.rec_on = False
 		
 	def __init__(self, master=None):
 		Frame.__init__(self, master)
@@ -223,5 +267,6 @@ class Application(Frame):
 
 root = Tk()
 app = Application(master=root)
+app.configure(bg="grey85")
 app.mainloop()
 root.destroy()
