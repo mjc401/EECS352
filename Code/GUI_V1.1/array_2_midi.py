a724da8a71ba79dcd013f78df5fa4d356b05d545
mjcollins <MatthewCollins2016@u.northwestern.edu>
2016-03-13 17:08:38 -0500

    Fixed midi segment issue

--- Code/GUI_V1.1/array_2_midi.py
+++ Code/GUI_V1.1/array_2_midi.py
@@ -20,7 +20,7 @@ def array_to_MIDI(array):
 		# add MIDI events
 		track.append(Message('program_change', program=12))
 		
-		seg_skip = 1
+		seg_skip_on = 1
 		
 		# take segments and make midi note events
 		for segment in xrange(0,array.shape[0]):
@@ -28,10 +28,11 @@ def array_to_MIDI(array):
 				track.append(Message('note_on', note=array[0,0], velocity=array[0,3], time=int(array[0,1]*512./44100*960)))
 				track.append(Message('note_off', note=array[0,0], velocity=array[0,3], time=test_duration[0]))
 			else:
-				track.append(Message('note_on', note=array[segment,0], velocity=array[segment,3], time=test_duration[seg_skip]))
-				track.append(Message('note_off', note=array[segment,0], velocity=array[segment,3], time=test_duration[seg_skip + 1]))
-				print test_duration[seg_skip + 1]
-				seg_skip = segment + 2
+				track.append(Message('note_on', note=array[segment,0], velocity=array[segment,3], time=test_duration[seg_skip_on]))
+				seg_skip_off = seg_skip_on + 1
+				track.append(Message('note_off', note=array[segment,0], velocity=array[segment,3], time=test_duration[seg_skip_off]))
+				print test_duration[seg_skip_off]
+				seg_skip_on = seg_skip_off + 1
 				
 		outfile.save('output.mid') # output MIDI file
 		
