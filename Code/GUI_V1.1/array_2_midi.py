a68a7ec7e4f47fb4074d3ac6a2802f4fbae5c35d
mjcollins <MatthewCollins2016@u.northwestern.edu>
2016-03-13 17:28:11 -0500

    Updated GUI
    Error in output midi file length

--- Code/GUI_V1.1/array_2_midi.py
+++ Code/GUI_V1.1/array_2_midi.py
@@ -6,13 +6,12 @@ from mido import Message, MidiFile, MidiTrack
 def array_to_MIDI(array):
 
 	# get relative times between events
-	test_duration = (np.diff(np.sort(np.concatenate((array[:,1],array[:,2])))* 512./44100*960)).astype(int)
+	test_duration = (np.diff(np.sort(np.concatenate((array[:,1],array[:,2])))* 512./44100*1000)).astype(int)
 	print array
 	print test_duration
 	#* 512./44100*960)
 	# write MIDI file
-	with MidiFile() as outfile:
-	
+	with MidiFile(ticks_per_beat = 500) as outfile:
 		# Initialize
 		track = MidiTrack()
 		outfile.tracks.append(track)
@@ -25,7 +24,7 @@ def array_to_MIDI(array):
 		# take segments and make midi note events
 		for segment in xrange(0,array.shape[0]):
 			if segment == 0:
-				track.append(Message('note_on', note=array[0,0], velocity=array[0,3], time=int(array[0,1]*512./44100*960)))
+				track.append(Message('note_on', note=array[0,0], velocity=array[0,3], time=int(array[0,1]*512./44100*1000)))
 				track.append(Message('note_off', note=array[0,0], velocity=array[0,3], time=test_duration[0]))
 			else:
 				track.append(Message('note_on', note=array[segment,0], velocity=array[segment,3], time=test_duration[seg_skip_on]))
